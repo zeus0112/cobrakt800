8482d97bdb0fa68070d479de0633c9430b7ad677
zeus0112 <113149207+zeus0112@users.noreply.github.com>
2023-07-18 18:45:14 -0600

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,6 +1,6 @@
 function addUser(){
     user_name=document.getElementById("user_name").value;
     localStorage.setItem("user_name", user_name);
-    windou.location="kwitter_room.html";
-    
+    window.location="kwitter_room.html";
+
 }
